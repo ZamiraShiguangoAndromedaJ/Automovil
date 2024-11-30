b07b55db9582b814eeeee51e346c6f7857a5a1ea
anthonypulupa <anthonypulupa2@gmail.com>
2024-11-30 18:30:37 -0500

    Automovil

--- src/main/java/zamira/estudiante/auto/Auto.java
+++ src/main/java/zamira/estudiante/auto/Auto.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package zamira.estudiante.auto;
 
@@ -9,8 +6,53 @@ package zamira.estudiante.auto;
  * @author user
  */
 public class Auto {
+     
+    public int velocidad; 
+
+  
+    public Auto() {
+        this.velocidad = 0;
+    }
+
+    public int acelerar(int incremento) {
+        velocidad += incremento; 
+        return velocidad;
+    }
+
+    public void desacelerar(int decrecimiento) {
+        velocidad = Math.max(0, velocidad - decrecimiento); 
+    }
+
+    public void frenar() {
+        velocidad = 0; 
+    }
+
+    public double tiempoLlegada(double distancia) {
+        if (velocidad == 0) {
+            return Double.POSITIVE_INFINITY; 
+        }
+        return distancia / velocidad; 
+    }
+
+    public int getvelocidadActual() {
+        return velocidad;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Auto auto = new Auto(); 
+
+        System.out.println("Velocidad inicial: " + auto.getvelocidadActual() + " km/h");
+
+
+        auto.acelerar(120); 
+        System.out.println("Velocidad tras acelerar: " + auto.getvelocidadActual() + " km/h");
+
+        auto.desacelerar(20); 
+        System.out.println("Velocidad tras desacelerar: " + auto.getvelocidadActual() + " km/h");
+
+        auto.frenar(); 
+        System.out.println("Velocidad tras frenar: " + auto.getvelocidadActual() + " km/h");
     }
 }
+
+
