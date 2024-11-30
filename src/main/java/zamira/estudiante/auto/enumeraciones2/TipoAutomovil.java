2dccae2f52753c02381b8b280f728850e341440b
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-30 15:02:26 -0500

    los metodos, get/set

---
+++ src/main/java/zamira/estudiante/auto/enumeraciones2/TipoAutomovil.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zamira.estudiante.auto.enumeraciones2;
+
+/**
+ *
+ * @author user
+ */
+public enum TipoAutomovil {
+    
+    Subcompacto,
+    Compacto,
+    Familiar,
+    Ejecutivo,
+    SUV
+    
+}
---
+++ src/main/java/zamira/estudiante/auto/enumeraciones3/TipoColor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zamira.estudiante.auto.enumeraciones3;
+
+/**
+ *
+ * @author user
+ */
+public enum TipoColor {
+    
+    Blanco,
+    Negro,
+    Naranja,
+    Amarillo,
+    Verde,
+    Azul,
+    Violeta
+    
+}
